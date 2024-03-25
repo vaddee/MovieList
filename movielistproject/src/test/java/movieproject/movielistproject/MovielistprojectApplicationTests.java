be5f6223fab695ea37dfd6026c7ec4e9144f6bec
vaddee <valtteri.laakso@hotmail.fi>
2024-03-25 11:08:34 +0200

    Projekti rakenne tehty

---
+++ movielistproject/src/test/java/movieproject/movielistproject/MovielistprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package movieproject.movielistproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielistprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
